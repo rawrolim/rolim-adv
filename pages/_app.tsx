e254b9365d5d7115b7fdadc5b06f4e7b14bb2c26
Rawlinson Rolim <alemaorolim@gmail.com>
2024-05-28 12:12:45 -0300

    INIT

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import { ToastContainer } from 'react-toastify';
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.min.js");
+  }, []);
+
+  return (
+    <>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
