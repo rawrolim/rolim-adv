733c81571b097a4207ca05091de81483c0e94855
Rawlinson Rolim <alemaorolim@gmail.com>
2024-05-29 16:17:04 -0300

    Correct middleware security

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,16 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { query } from './config/databaseConnection';
 import axios from 'axios';
 
 export async function middleware(request: NextRequest) {
     const authHeader = request.headers.get('authorization');
-    let unAuthorized = true;
-    
-    if (authHeader) {
-        if(authHeader == process.env.API_KEY){
-            unAuthorized = false;
-        }else{
+    let Authorized = false;
 
+    if (authHeader) {
+        console.log(request.nextUrl)
+        if((request.nextUrl.pathname == '/api/login' || request.nextUrl.pathname == '/api/checkHeader') && authHeader == process.env.API_KEY){
+            Authorized = true;
+        }else{
             const res = await axios.post("http://localhost:3000/api/checkHeader", {
                 authorization: authHeader
             }, {
@@ -19,11 +18,11 @@ export async function middleware(request: NextRequest) {
                     "Authorization": process.env.API_KEY
                 }
             });
-            unAuthorized = !res.data.accepted;
+            Authorized = res.data.accepted;
         }
     }
 
-    if(unAuthorized){
+    if(!Authorized){
         return new NextResponse(JSON.stringify({ error: 'Unauthorized' }), {
             status: 401,
             headers: {
