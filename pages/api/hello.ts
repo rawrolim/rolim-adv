22f33630c275a940900783670cfb166d410165a7
PedroRolim <38291493+pprolim@users.noreply.github.com>
2024-05-29 21:37:40 -0300

    update docker-compose to remove version and show the database info on screen

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,7 +4,7 @@ import db from "../../config/databaseConnection";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const clientes = await db.query("SELECT * FROM clientes");
 
-  console.log(clientes)
+  console.log(clientes);
   
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(clientes);
 }
