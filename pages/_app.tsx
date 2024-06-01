fc2a55fb2a14c3f9b54104bbfe8cc8e532277ef6
Danreby <112499709+Danreby@users.noreply.github.com>
2024-06-01 17:40:13 -0300

    Tela de Login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ToastContainer } from 'react-toastify';
 import '../styles/globals.css'
+import '../styles/LoginPage.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import LoginPage from './LoginPage'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        INDEX
+        <a href='./LoginPage'>Login</a>
       </main>
     </div>
   )
