6bdcea0f423dad85a2298d0416823f20aa361f11
Rolimm <gabrielmrolim201@gmail.com>
2024-06-02 14:43:51 -0300

    feat-Index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { ToastContainer } from 'react-toastify';
 import '../styles/globals.css'
 import '../styles/navbar.css'
-import '../styles/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react';
