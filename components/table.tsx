67c57cf2daad876adc0f5ab80e34b8b2dfcebbe5
PedroRolim <38291493+pprolim@users.noreply.github.com>
2024-06-02 18:08:35 -0300

    more components to dashboard and included filters

--- components/table.tsx
+++ components/table.tsx
@@ -30,8 +30,39 @@ export function Table({ data, title, columns }) {
             </tr>
             <tr>
               <th scope="row">3</th>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">5</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">6</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">7</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">8</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
             </tr>
           </tbody>
         </table>
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,9 +3,10 @@ import pdfHipo from "./pdf";
 import http from "../config/http";
 import useLoacalStorage from "../hooks/useLocalStorage";
 import { useEffect } from "react";
-import Card from "../components/Card";
+import Card from "../components/card";
 import Chart from "../components/chart";
 import Table from "../components/table";
+import Filter from "../components/filter";
 
 export default function Dashboard() {
   const [token, setToken] = useLoacalStorage("authorization", "");
@@ -31,35 +32,55 @@ export default function Dashboard() {
       </Head>
 
       <main>
-        <div className="container-full text-center m-5">
-          <div className="row my-4">
-            <div className="col mx-2">
-              <Card data={"32"} cardTitle={"Clientes Ativos"} />
-            </div>
-            <div className="col mx-2">
-              <Card data={"32"} cardTitle={"Clientes Ativos"} />
-            </div>
-            <div className="col mx-2">
-              <Card data={"32"} cardTitle={"Clientes Ativos"} />
-            </div>
-            <div className="col mx-2">
-              <Card data={"32"} cardTitle={"Clientes Ativos"} />
-            </div>
-          </div>
+        <div className="container-full text-center p-5 bg-body-tertiary">
+        <h1 className="text-center">Dashboard</h1>
           <div className="row">
-            <div className="col-6 mx-3">
-              <Chart
-                data={chartDataExample}
-                title={"Processos por mês"}
-                type={"line"}
-              />
+            <div className="col-md-2 bg-body-secondary border-5">
+                <div className="container-full">
+                <h3 className="m-4">Filtros</h3>
+                <Filter data={["10"]} title={"Selecione o mês"} />
+                <Filter data={["10"]} title={"Selecione o ano"} />
+                <Filter data={["10"]} title={"Selecione o advogado"} />
+                <Filter data={["10"]} title={"Selecione o cliente"} />
+                </div>
             </div>
-            <div className="col-5 mx-3">
-              <Table
-                data={"test"}
-                title={"Últimas atualizações"}
-                columns={["ID", "Data", "Processo", "Advogado", "Atualização"]}
-              />
+            <div className="col-md-10">
+              <div className="row my-4">
+                <div className="col mx-2">
+                  <Card data={"32"} cardTitle={"Clientes Ativos"} />
+                </div>
+                <div className="col mx-2">
+                  <Card data={"152"} cardTitle={"Processos Ativos"} />
+                </div>
+                <div className="col mx-2">
+                  <Card data={"265"} cardTitle={"Total de Clientes"} />
+                </div>
+                <div className="col mx-2">
+                  <Card data={"7592"} cardTitle={"Total de Processos"} />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-6 mx-3">
+                  <Chart
+                    data={chartDataExample}
+                    title={"Processos por mês"}
+                    type={"line"}
+                  />
+                </div>
+                <div className="col-5 mx-3">
+                  <Table
+                    data={"test"}
+                    title={"Últimas atualizações"}
+                    columns={[
+                      "ID",
+                      "Data",
+                      "Processo",
+                      "Advogado",
+                      "Atualização",
+                    ]}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
