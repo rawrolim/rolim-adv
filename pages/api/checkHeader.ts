fa2ff02673a215e33c3a6830761c4578b60448c2
Rawlinson Rolim <alemaorolim@gmail.com>
2024-06-07 14:57:07 -0300

    Página para trocar a senha do usuário no primeiro acesso

--- pages/api/checkHeader.ts
+++ pages/api/checkHeader.ts
@@ -9,7 +9,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const rs_user = await query(`SELECT * FROM usuarios WHERE token = '${authHeader}'`);
         if (rs_user.length > 0) {
             jwt.verify(authHeader,process.env.JWT_SECRET);
-
+            
+            const tokenData = jwt.decode(authHeader)
+            const rs_user2 = await query(`SELECT * FROM usuarios WHERE token = '${authHeader}' AND id = '${tokenData.user_id}'`);
+            if(rs_user2.length > 0){
+                res.status(200).json({ accepted: true })
+            }else{
+                res.status(200).json({ accepted: false })
+            }
             res.status(200).json({ accepted: true })
         } else {
             res.status(200).json({ accepted: false })
--- pages/senha/novo.tsx
+++ pages/senha/novo.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import styles from '../../styles/LoginPage.module.css';
 import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+import http from '../../config/http';
 
 export default function SenhaNova() {
     const [password, setPassword] = useState("");
@@ -12,8 +14,8 @@ export default function SenhaNova() {
         user_access_name: "",
         user_first_access: true,
         user_access_master: false,
-        exp: 1717631199,
-        iat: 1717627599,
+        exp: 0,
+        iat: 0,
         refreshToken: ""
     });
     const router = useRouter();
@@ -25,10 +27,22 @@ export default function SenhaNova() {
         }
     }, []);
 
-    console.log(userData)
-
     async function save() {
+        try{
+            if(password == '' || passwordRepit == '')
+                throw new Error("As duas senhas devem ter valor.")
+                
+            if(password != passwordRepit)
+                throw new Error("As senha inseridas devem ser iguais.")
 
+            const res = await http.post('/api/newPassword',{
+                password
+            });
+            toast.success("Senha atualizada com sucesso")
+            router.push("/dashboard")
+        }catch(err){
+            toast.error(err.toString());
+        }
     }
 
     return (
@@ -36,13 +50,13 @@ export default function SenhaNova() {
             <main className='container-fluid'>
                 <div className={`${styles.inputGroup} shadow col-11 col-sm-10 col-md-8 col-lg-4`}>
                     <div className='col-12 d-flex'>
-                        <h3 className={/*styles.LoginTitle*/ ' align-self-center col-6 text-white p-2 pt-3 text-start'}>Login</h3>
+                        <h3 className={/*styles.LoginTitle*/ ' align-self-center col-6 text-white p-2 pt-3 text-start'}>Primeiro acesso</h3>
                         <div className='text-end col-6 align-self-center'>
-                            <button className='btn text-white' onClick={() => router.push('/')}>Voltar</button>
+                            <button className='btn text-white' onClick={() => router.push('/login')}>Voltar</button>
                         </div>
                     </div>
-                    <div>
-                        {userData.user_name}
+                    <div className='text-white'>
+                        Bem vindo, {userData.user_name}. Insira sua nova senha
                     </div>
                     <div className={styles.input}>
                         <input
@@ -51,6 +65,18 @@ export default function SenhaNova() {
                             value={password}
                             placeholder="Senha"
                             className={'form-control'}
+                            onChange={e=>setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className={styles.input}>
+                        <input
+                            type="password"
+                            id="password"
+                            value={passwordRepit}
+                            placeholder="Repita a senha"
+                            className={'form-control'}
+                            onChange={e=>setPasswordRepit(e.target.value)}
                             required
                         />
                     </div>
