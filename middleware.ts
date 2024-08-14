4b05a85a14b2215fec68ce537bd5c34a34887751
Rawlinson Rolim <alemaorolim@gmail.com>
2024-08-14 12:33:33 -0300

    correct checkHeader request

--- middleware.ts
+++ middleware.ts
@@ -20,7 +20,7 @@ export async function middleware(request: NextRequest) {
             Authorized = true;
         }else{
             try{
-                const res = await axios.post("http://localhost:3000/api/checkHeader", {
+                const res = await axios.post("/api/checkHeader", {
                     authorization: authHeader
                 }, {
                     headers: {
