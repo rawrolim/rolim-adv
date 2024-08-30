61c68786b63647bd2ea2fd7f1da25fe96e75156a
PedroRolim <38291493+pprolim@users.noreply.github.com>
2024-08-30 17:09:47 -0300

    dashboard backend

--- pages/api/dashboard/index.ts
+++ pages/api/dashboard/index.ts
@@ -4,10 +4,32 @@ import { query } from "../../../config/databaseConnection";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try{
         if(req.method == 'GET'){
+            let responseObject = {
+                clientesAtivos: 0,
+                clientesTotal: 0,
+                processosAtivos: 0,
+                usuariosAtivos: 0,
+                Advogados: []
+            };
+
             let sql = `
             SELECT * FROM clientes`
             const rs_clientes = await query(sql);
-            res.status(200).json(rs_clientes);
+            responseObject.clientesAtivos = rs_clientes.filter(c => c.status = 'A').length;
+            responseObject.clientesTotal = rs_clientes.length;
+
+            sql = `
+            SELECT * FROM processos`
+            const rs_processos = await query(sql);
+            responseObject.processosAtivos = rs_processos.length;
+
+            sql = `
+            SELECT * FROM usuarios WHERE status = 'A'`
+            const rs_usuarios = await query(sql);
+            responseObject.usuariosAtivos = rs_usuarios.length - 2;
+            responseObject.Advogados = rs_usuarios.filter(u => u.tipo_usuario != 4 && u.tipo_usuario != 5);
+
+            res.status(200).json(responseObject);
         }else{
             throw new Error("Method not allowed")
         }
