2a6d46ed33e2fdb4196dadbfc663c17717e81369
Rawlinson Rolim <alemaorolim@gmail.com>
2025-01-27 14:36:17 -0300

    DONE: download file

--- pages/api/servidor.ts
+++ pages/api/servidor.ts
@@ -1,15 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { query } from "../../config/databaseConnection";
 import axios from "axios";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try{
-        const serverResponse = await axios.get(`${process.env.FILESERVER}/getDirectory`, {
-            headers: {
-                Authorization: req.headers.authorization
+        if(req.method == 'GET'){
+            if(req.query.path){
+                const serverResponse = await axios.get(`${process.env.FILESERVER}/getFile?key=${req.query.path}`, {
+                    headers: {
+                        Authorization: req.headers.authorization
+                    }
+                });
+                res.status(200).json(serverResponse.data);
+            }else{
+                const serverResponse = await axios.get(`${process.env.FILESERVER}/getDirectory`, {
+                    headers: {
+                        Authorization: req.headers.authorization
+                    }
+                });
+                res.status(200).json(serverResponse.data);
             }
-        });
-        res.status(200).json(serverResponse.data);
+        }
+        
     }catch(err){
         res.status(400).json(err.toString())
     }
--- pages/servidor/index.tsx
+++ pages/servidor/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import http from "../../config/http";
 import { useEffect, useState } from "react";
 import { FaFile, FaFolder, FaFolderOpen } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 export default function Servidor() {
     const router = useRouter();
@@ -13,52 +14,65 @@ export default function Servidor() {
 
     async function getDirectory() {
         const res = await http.get(`/api/servidor`);
-        console.log(res)
         setDir(res);
     }
 
-    const DirectoryList = ({ items }) => {
+    const DirectoryList = ({ items, father }) => {
         return (
             <ul className="list-group">
                 {items.map((item, index) => (
-                    <DirectoryItem key={index} item={item} />
+                    <DirectoryItem key={index} item={item} father={father ? father : ''} />
                 ))}
             </ul>
         );
     };
-    
-    const DirectoryItem = ({ item }) => {
+
+    const DirectoryItem = ({ item, father }) => {
         const [isOpen, setIsOpen] = useState(false);
-    
+
         const toggleOpen = () => {
             setIsOpen(!isOpen);
         };
-    
+
         return (
             <li className="list-group-item">
                 {item.type === 'directory' ? (
                     <>
                         <span onClick={toggleOpen} style={{ cursor: 'pointer' }}>
-                            {isOpen?<FaFolderOpen />: <FaFolder />}
-                            <strong>{item.name}</strong>
+                            {isOpen ? <FaFolderOpen /> : <FaFolder />}
+                            <strong className="ms-1">{item.name}</strong>
                         </span>
-                        {isOpen && <DirectoryList items={item.items} />}
+                        {isOpen && <DirectoryList items={item.items} father={father + '/' + item.name} />}
                     </>
                 ) : (
                     <>
                         <FaFile />
-                        {item.name}
+                        <a className='ms-1' onClick={() => downloadItem(father + '/' + item.name)}>{item.name}</a>
                     </>
                 )}
             </li>
         );
     };
 
+    const downloadItem = async (path: string) => {
+        const pathArr = path.split('/');
+        const base64Data = await http.get(`/api/servidor?path=${path}`)
+        if(base64Data){
+            const linkSource = `data:application/octet-stream;base64,${base64Data}`;
+            const downloadLink = document.createElement('a');
+            downloadLink.href = linkSource;
+            downloadLink.download = pathArr[pathArr.length-1];
+            downloadLink.click();
+        }else{
+            toast.warn("O arquivo solicitado está vazio.")
+        }
+    }
+
     return (
         <div>
             <main className="mt-3" >
                 <button className='btn btn-outline-light text-dark' onClick={getDirectory}>Get directory</button>
-                <DirectoryList items={dir} />
+                <DirectoryList items={dir} father={''} />
             </main>
         </div>
     )
